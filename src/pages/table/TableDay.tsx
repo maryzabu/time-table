a5fde09652f2b3a9b645682ca74f0ba430f1bcf9
Ivan Zabunyan <ivanes@list.ru>
2022-12-11 20:21:36 +0300

    correct

--- src/pages/table/TableDay.tsx
+++ src/pages/table/TableDay.tsx
@@ -1,42 +1,51 @@
 import {Table} from "antd";
+import {ReactElement} from "react";
 
 const columns = [
   {
     title: <>Класс /<br/>Время</>,
     dataIndex: 'time',
     key: 'time',
-    render: (time: string) => <strong>{time}</strong>
+    render: (time: string) => <strong>{time}</strong>,
+    className: 'first'
+
   },
   {
     title: '10 г',
     dataIndex: 'c10g',
     key: 'c10g',
+    render: renderBr,
   },
   {
     title: '10 сэ',
     dataIndex: 'c10se',
     key: 'c10se',
+    render: renderBr,
   },
   {
     title: '10 т',
     dataIndex: 'c10t',
     key: 'c10t',
+    render: renderBr,
   },
   {
     title: '11 г',
     dataIndex: 'c11g',
     key: 'c11g',
+    render: renderBr,
   },
   {
     title: '11 сэ',
     dataIndex: 'c11se',
     key: 'c11se',
+    render: renderBr,
   },
 
   {
     title: '11 т',
     dataIndex: 'c11t',
     key: 'c11t',
+    render: renderBr,
   },
 ];
 
@@ -54,5 +63,9 @@ type Props = {
 }
 
 export function TableDay({date, data}: Props) {
-  return (<Table columns={columns} title={() => date} dataSource={data} pagination={false}/>);
+  return (<Table columns={columns} title={() => date} dataSource={data} pagination={false} className={'table-day'}/>);
+}
+
+function renderBr(text: string): ReactElement {
+  return text.split('\n').reduce((prev: ReactElement, text) => <>{prev}{text}<br/></>, <></>);
 }
